251a23ae33d0cd61bf986309f2b10108a5c1bd29
ImperialCrowns <maxime.dziura@epitech.eu>
2024-09-23 17:03:32 +0900

    feat(front): Add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages";
+import Projects from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="dashboard" element={<Projects />} />
         {/* <Route path="/github" element={<GithubCallback />} />
         <Route element={<AuthenticatedGuard />}>
           <Route element={<Layout />}>
