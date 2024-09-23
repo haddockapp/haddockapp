6315718d4e61558f9b1f09fd49074cadf0543789
agherasie <alexandru.gherasie@epitech.eu>
2024-09-23 16:01:04 +0900

    feat(front): init front

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { FC } from "react";
+
+const Home: FC = () => (
+  <div className="h-screen justify-center items-center flex flex-col space-y-4">
+    <div className="space-y-0">
+      <img className="w-80" src="./haddock.png" />
+      <h1 className="text-4xl text-center">Welcome</h1>
+    </div>
+    <Button
+      onClick={() =>
+        (window.location.href = `https://github.com/login/oauth/authorize?client_id=${1}&scope=user%20repo`)
+      }
+      className="p-4 gap-2"
+    >
+      <div className="devicon-github-original" />
+      <span className="text-white font-semibold">Signup with Github</span>
+    </Button>
+  </div>
+);
+
+export default Home;
