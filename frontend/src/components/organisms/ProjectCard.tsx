b33319b287a5b3c5d90053cf2a1ef21f68dfaced
ImperialCrowns <maxime.dziura@epitech.eu>
2024-09-30 16:03:30 +0900

    fix(frontend): Remove deprecated function

--- frontend/src/components/organisms/ProjectCard.tsx
+++ frontend/src/components/organisms/ProjectCard.tsx
@@ -20,7 +20,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
     return <FolderDot size={64} />;
   };
   const truncate = (str: string, n: number) => {
-    return str.length > n ? str.substr(0, n - 1) + "..." : str;
+    return str.length > n ? str.substring(0, n - 1) + "..." : str;
   };
   const description =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
