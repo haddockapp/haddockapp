9e19964980eee27042a50dd066c4c38cbaad5060
agherasie <alexandru.gherasie@epitech.eu>
2024-10-17 12:20:59 +0900

    feat: code splitting domain step

---
+++ frontend/src/components/organisms/DomainSetup/DomainNameForm.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
+import {
+  DomainResponseDto,
+  useCreateDomainMutation,
+} from "@/services/backendApi/domains";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  domain: z.string().regex(/^[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}$/, {
+    message: "Invalid domain name",
+  }),
+});
+
+interface DomainNameFormProps {
+  domain?: DomainResponseDto;
+  main?: boolean;
+}
+const DomainNameForm: FC<DomainNameFormProps> = ({ domain, main }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      domain: domain?.domain || "",
+    },
+  });
+
+  const [triggerCreateDomain] = useCreateDomainMutation();
+
+  const onSubmit = form.handleSubmit((data) => {
+    triggerCreateDomain({ domain: data.domain, main: main ?? false })
+      .unwrap()
+      .then((d) => {
+        toast({ title: "Domain created", description: JSON.stringify(d) });
+        form.reset();
+      })
+      .catch((e) => {
+        toast({ title: "Error", description: JSON.stringify(e) });
+      });
+  });
+
+  return (
+    <Form {...form}>
+      <form className="text-start" onSubmit={onSubmit}>
+        <div className="flex space-x-4">
+          <FormField
+            control={form.control}
+            name="domain"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input {...field} placeholder="Domain name" />
+                </FormControl>
+                <FormDescription className="italic">
+                  eg: example.com, domain.fr...
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Confirm</Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default DomainNameForm;
---
+++ frontend/src/components/organisms/DomainSetup/index.tsx
@@ -0,0 +1,50 @@
+import {
+  DomainResponseDto,
+  useGetDomainStatusQuery,
+  useDeleteDomainMutation,
+} from "@/services/backendApi/domains";
+import { FC } from "react";
+import DomainNameForm from "./DomainNameForm";
+import DomainSetupStep from "./DomainSetupStep";
+import steps from "./domainSteps";
+import DomainActions from "./DomainActions";
+
+interface SetupDomainFormProps {
+  domain?: DomainResponseDto;
+  main?: boolean;
+}
+const SetupDomainForm: FC<SetupDomainFormProps> = ({ domain, main }) => {
+  const { data, refetch, isFetching } = useGetDomainStatusQuery(
+    domain?.id ?? "",
+    {
+      skip: !domain,
+    }
+  );
+
+  const [triggerDeleteDomain] = useDeleteDomainMutation();
+
+  return (
+    <div className="px-8 pt-1 space-y-6">
+      {!domain && <DomainNameForm domain={domain} main={main} />}
+      {steps.map((s) => (
+        <DomainSetupStep
+          key={s.title}
+          step={s}
+          domain={domain}
+          completed={data?.[s.boolean] as boolean}
+        />
+      ))}
+      <div className="flex space-x-2">
+        <DomainActions
+          isRefreshing={isFetching}
+          onRefresh={refetch}
+          onDelete={triggerDeleteDomain}
+          onSave={() => {}}
+          status={data}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SetupDomainForm;
