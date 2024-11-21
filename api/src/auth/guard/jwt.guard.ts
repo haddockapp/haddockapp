5f36b2f976132ea9f9ad76685346bd6be0d599dd
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-11-21 17:34:34 +0900

    feat: auth guard for github configuration to make endpoint public when app is not configured yet and private when it is

--- api/src/auth/guard/jwt.guard.ts
+++ api/src/auth/guard/jwt.guard.ts
@@ -1,20 +1,40 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { IS_PUBLIC_KEY } from '../auth.decorator';
+import { IS_PUBLIC_CONFIG_KEY, IS_PUBLIC_KEY } from '../auth.decorator';
+import { ConfigurationRepository } from 'src/configuration/configuration.repository';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    constructor(private reflector: Reflector) {
+    constructor(private reflector: Reflector, private readonly configurationRepository: ConfigurationRepository) {
         super();
     }
 
-    public canActivate(context: ExecutionContext) {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+    private isPublicEndpoint(context: ExecutionContext) {
+        return this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+    }
+
+    private async isAppConfigured(context: ExecutionContext) {
+        const isPublicConfig = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_CONFIG_KEY, [
             context.getHandler(),
             context.getClass(),
         ]);
 
-        return isPublic || super.canActivate(context);
+        if (!isPublicConfig) return false;
+
+        const config = await this.configurationRepository.getConfigurationNoThrow();
+        return config && config.configured;
+    }
+
+    public async canActivate(context: ExecutionContext) {
+        const isPublic = this.isPublicEndpoint(context);
+        const isAppConfigured = await this.isAppConfigured(context);
+
+        if (isPublic) return true;
+        if (!isAppConfigured) return true;
+        return super.canActivate(context) as boolean;
     }
 }
