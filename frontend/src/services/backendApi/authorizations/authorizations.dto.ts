a3bd207dff3cdb6e2151137179d6675441582aec
agherasie <alexandru.gherasie@epitech.eu>
2025-01-21 01:27:06 +0900

    hotfix: name in authorization payload

--- frontend/src/services/backendApi/authorizations/authorizations.dto.ts
+++ frontend/src/services/backendApi/authorizations/authorizations.dto.ts
@@ -1,27 +1,34 @@
 export enum AuthorizationEnum {
-    OAUTH = 'oauth',
-    PERSONAL_ACCESS_TOKEN = 'personal_access_token',
-    DEPLOY_KEY = 'deploy_key'
+  OAUTH = "oauth",
+  PERSONAL_ACCESS_TOKEN = "personal_access_token",
+  DEPLOY_KEY = "deploy_key",
 }
 
 export type Authorization = {
-    id: string;
-    type: AuthorizationEnum;
-}
+  id: string;
+  name: string;
+  type: AuthorizationEnum;
+};
 
-export type AuthorizationCreateDto = {
-    type: AuthorizationEnum.DEPLOY_KEY;
-    data: {
+export type AuthorizationCreateDto =
+  | {
+      name: string;
+      type: AuthorizationEnum.DEPLOY_KEY;
+      data: {
         key: string;
+      };
     }
-} | {
-    type: AuthorizationEnum.OAUTH;
-    data: {
+  | {
+      name: string;
+      type: AuthorizationEnum.OAUTH;
+      data: {
         code: string;
+      };
     }
-} | {
-    type: AuthorizationEnum.PERSONAL_ACCESS_TOKEN;
-    data: {
+  | {
+      name: string;
+      type: AuthorizationEnum.PERSONAL_ACCESS_TOKEN;
+      data: {
         token: string;
-    }
-}
+      };
+    };
