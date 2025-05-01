05b50e1e9d6d146f1b60d4f9eb31d86a5f325e2e
agherasie <alexandru.gherasie@epitech.eu>
2025-05-01 23:35:57 +0900

    feat: histograms

--- frontend/src/components/organisms/ProjectTabs/MonitoringTab/Histograms.tsx
+++ frontend/src/components/organisms/ProjectTabs/MonitoringTab/Histograms.tsx
@@ -1,51 +1,60 @@
 import { Histogram } from "@/components/ui/charts/histogram";
 import { CpuIcon, DiscIcon, MemoryStickIcon } from "lucide-react";
 import { FC } from "react";
-import { MetricData } from ".";
+// import cpuUsage from "./cpuUsage.json";
+// import diskUsage from "./diskUsage.json";
+// import memoryUsage from "./memoryUsage.json";
+import { CpuUsage, DiskUsage, MemoryUsage } from ".";
 
 const Histograms: FC<{
-  diskData: MetricData[];
-  memoryData: MetricData[];
-  cpuData: MetricData[];
+  diskData: DiskUsage[];
+  memoryData: MemoryUsage[];
+  cpuData: CpuUsage[];
 }> = ({ diskData, memoryData, cpuData }) => (
   <>
     <Histogram
+      domain={[0, 100]}
       config={{
         y: {
           label: "CPU Usage",
+          unit: "%",
           color: "hsl(var(--chart-2))",
           icon: CpuIcon,
         },
       }}
       data={cpuData.map((data) => ({
         x: new Date(data.timestamp).toString(),
-        y: data.value,
+        y: data.percent,
       }))}
     />
     <Histogram
+      domain={[0, +(memoryData[0]?.total / 1024 ** 2).toFixed(0)]}
       config={{
         y: {
-          label: "Memory Usage",
+          label: `Memory Usage`,
+          unit: "MB",
           color: "hsl(var(--chart-1))",
           icon: MemoryStickIcon,
         },
       }}
       data={memoryData.map((data) => ({
         x: new Date(data.timestamp).toString(),
-        y: data.value,
+        y: (data.total - data.available) / 1024 ** 2,
       }))}
     />
     <Histogram
+      domain={[0, +(diskData[0]?.total / 1024 ** 3).toFixed(0)]}
       config={{
         y: {
           label: "Disk Usage",
+          unit: "GB",
           color: "hsl(var(--chart-4))",
           icon: DiscIcon,
         },
       }}
       data={diskData.map((data) => ({
         x: new Date(data.timestamp).toString(),
-        y: data.value,
+        y: data.used / 1024 ** 3,
       }))}
     />
   </>
--- frontend/src/components/organisms/ProjectTabs/MonitoringTab/index.tsx
+++ frontend/src/components/organisms/ProjectTabs/MonitoringTab/index.tsx
@@ -12,16 +12,30 @@ import LogsSection from "./LogsSection";
 import UsageCharts from "./UsageCharts";
 import Histograms from "./Histograms";
 
-export type MetricData = {
+export type CpuUsage = {
+  user: number;
+  system: number;
+  idle: number;
   percent: number;
+
   timestamp: Date;
-  used?: number;
-  total?: number;
-  free?: number;
-  idle?: number;
-  system?: number;
-  user?: number;
-  available?: number;
+};
+
+export type MemoryUsage = {
+  total: number;
+  available: number;
+  percent: number;
+
+  timestamp: Date;
+};
+
+export type DiskUsage = {
+  total: number;
+  percent: number;
+  used: number;
+  free: number;
+
+  timestamp: Date;
 };
 
 const MonitoringTab: FC = () => {
@@ -29,9 +43,9 @@ const MonitoringTab: FC = () => {
 
   const { data: me } = useGetSelfQuery();
 
-  const [cpuUsage, setCpuUsage] = useState<MetricData[]>([]);
-  const [diskUsage, setDiskUsage] = useState<MetricData[]>([]);
-  const [memoryUsage, setMemoryUsage] = useState<MetricData[]>([]);
+  const [cpuUsage, setCpuUsage] = useState<CpuUsage[]>([]);
+  const [diskUsage, setDiskUsage] = useState<DiskUsage[]>([]);
+  const [memoryUsage, setMemoryUsage] = useState<MemoryUsage[]>([]);
 
   const [logs, setLogs] = useState<string[]>([]);
 
@@ -93,12 +107,10 @@ const MonitoringTab: FC = () => {
     };
   }, [cpuUsage, diskUsage, me, memoryUsage, projectId]);
 
-  console.log(cpuUsage, diskUsage, memoryUsage);
-
   return (
-    <div className="px-8">
+    <div className="px-8 space-y-8">
       <h1 className="text-3xl font-bold mt-8 mb-4">Monitoring</h1>
-      <div className="grid grid-cols-3 justify-items-center">
+      <div className="grid grid-cols-3 w-fit mx-auto justify-items-center gap-x-12">
         <UsageCharts
           cpuUsage={cpuUsage[-1]?.percent}
           memoryUsage={memoryUsage[-1]?.percent}
