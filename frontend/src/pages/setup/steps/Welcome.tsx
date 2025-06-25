4c44e1c018215c59dba8d9c47b7fe4d7241fe0fe
agherasie <alexandru.gherasie@epitech.eu>
2025-06-25 19:36:03 +0200

    fix: github app infos

--- frontend/src/pages/setup/steps/Welcome.tsx
+++ frontend/src/pages/setup/steps/Welcome.tsx
@@ -1,7 +1,8 @@
 import Copiable from "@/components/atoms/copiable";
 import { Button } from "@/components/ui/button";
+import { useGetConfigurationQuery } from "@/services/backendApi/configuration";
 import { CircleGauge } from "lucide-react";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useMemo } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Welcome: FC = () => {
@@ -12,18 +13,29 @@ const Welcome: FC = () => {
     setSearchParams({ step: "welcome" });
   }, [setSearchParams]);
 
+  const { data: configurationData } = useGetConfigurationQuery();
+
+  const isGithubAppConfirmed = useMemo(
+    () => configurationData?.find((c) => c.key === "github_client_id"),
+    [configurationData]
+  );
+
   return (
     <div className="flex flex-col space-y-12 text-center items-center w-full text-gray-700">
-      <p className="flex flex-col">
-        <span>
-          Now that your domain is properly configured @ {window.location.origin}
-          ,
-        </span>
-        <span>
-          don't forget to edit your Github Application's callback url:
-        </span>
-      </p>
-      <Copiable text={`${window.location.origin}/github`} />
+      {isGithubAppConfirmed && (
+        <>
+          <p className="flex flex-col">
+            <span>
+              Now that your domain is properly configured @{" "}
+              {window.location.origin},
+            </span>
+            <span>
+              don't forget to edit your Github Application's callback url:
+            </span>
+          </p>
+          <Copiable text={`${window.location.origin}/github`} />
+        </>
+      )}
       <div>
         <Button
           variant="link"
